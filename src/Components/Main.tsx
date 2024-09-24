db8ceed8f9d809496ad4b71492c312559177566c
Yug Joshi <yugjoshi628@gmail.com>
2024-09-24 18:19:23 +0530

    started working on open_call website

---
+++ src/Components/Main.tsx
@@ -0,0 +1,37 @@
+import "@/CssFile/Main.css";
+
+function Main() {
+  return (
+    <main className="page home-page">
+      <div className="ai-act-wrap">
+        <div className="ai-bg -inview">
+          <span data-v-8302dbff=""></span>
+          <span data-v-8302dbff=""></span>
+          <span data-v-8302dbff=""></span>
+        </div>
+        <section className="c-welcome" data-v-8302dbff>
+          <div className="-w" data-v-8302dbff>
+            <div className="-w-in">
+              <h1 className="head-1">
+                <span data-v-8302dbff="">Your Own AI Call Center</span>
+              </h1>
+              <p data-v-8302dbff="" className="para-1">
+                OpenCall AI empowers your business, driving revenue, cutting
+                overhead, and streamlining all phone activities for seamless
+                operations.
+              </p>
+              <p>
+                <span data-v-8302dbff>
+                  <svg data-v-8302dbff path="@/CssFile/images/down-img.svg" />
+                  Answer Questions
+                </span>
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
