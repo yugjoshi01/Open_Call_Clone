f929123e7e2bc7bca78db75a359a872626a3621a
Yug Joshi <yugjoshi628@gmail.com>
2024-09-30 17:46:47 +0530

    add section three in project

--- src/Components/Main_Section/Main.tsx
+++ src/Components/Main_Section/Main.tsx
@@ -1,5 +1,6 @@
 import Sec1 from "../sec_1/Sec1";
 import Sec2 from "../Sec_2/Sec2";
+import Sec_3 from "../Sec_3/Sec_3";
 import styleMain from "./Main.module.css";
 
 function Main() {
@@ -7,6 +8,7 @@ function Main() {
     <main className={styleMain.main}>
       <Sec1 />
       <Sec2 />
+      <Sec_3 />
     </main>
   );
 }
---
+++ src/Components/Sec_3/Sec_3.tsx
@@ -0,0 +1,22 @@
+import style3 from "./Sec_3.module.css";
+
+function Sec_3() {
+  return (
+    <>
+      <section className={style3.c_promise} data-v-e2c0c49b data-v-5dd511ed>
+        <div className={style3.w} data-v-5dd511ed>
+          <figure className={style3.icon}>
+            <video className={style3.video} autoPlay muted loop>
+              <source src="./clock.mp4" type="video/mp4" />
+            </video>
+          </figure>
+          <span className={style3.tac} data-v-5dd511ed>
+            We'll Create It in Minutes
+          </span>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Sec_3;
