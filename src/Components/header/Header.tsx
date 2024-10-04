c2f9022a56e3e7031b2cec158f314fdf60bc1002
Yug Joshi <yugjoshi628@gmail.com>
2024-10-04 12:40:38 +0530

    add header section

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -1,7 +1,57 @@
-import style_header from "./Header.module.css";
+import Image from "next/image";
+import logo from "../../../public/images/logo.svg";
+import styleHead from "./Header.module.css";
 
 function Header() {
-  return <header className={style_header.header} data-v-568e58aa></header>;
+  return (
+    <header className={styleHead.header} data-v-568e58aa>
+      <div className={styleHead.w} data-v-568e58aa>
+        <a href="/" className={styleHead.logo} data-v-568e58aa>
+          <span className={styleHead.l_symbol} data-v-568e58aa>
+            <span data-v-568e58aa>
+              <Image src={logo} alt="logo" height={48.6} width={48.6} />
+            </span>
+          </span>
+          <span className={styleHead.l_tt} data-v-568e58aa>
+            <h1 className={styleHead.h1}>OpenCall</h1>
+            <h2 className={styleHead.h2}>.ai</h2>
+          </span>
+        </a>
+        <nav className={styleHead.nav} data-v-568e58aa>
+          <a href="/" data-v-568e58aa>
+            <span className={styleHead.base} data-v-568e58aa>
+              About
+            </span>
+          </a>
+          <a href="/" data-v-568e58aa>
+            <span className={styleHead.base} data-v-568e58aa>
+              Product
+            </span>
+          </a>
+          <a href="/" data-v-568e58aa>
+            <span className={styleHead.base} data-v-568e58aa>
+              Pricing
+            </span>
+          </a>
+        </nav>
+        <a
+          href="https://app.open-call.ai/api/auth/signup"
+          target="_blank"
+          className={`${styleHead.button} ${styleHead.p} ${styleHead.b}`}
+          data-v-e3fde6b2
+        >
+          <span className={styleHead.text} data-v-e3fde6b2>
+            <span data-label="Sign In" data-v-e3fde6b2>
+              <span data-v-e3fde6b2>Sign In</span>
+            </span>
+          </span>
+          <svg data-v-3089695e data-v-e3fde6b2-s className={styleHead.svg_icon}>
+            <use href="#Capa_2" data-v-3089695e data-v-e3fde6b2-s></use>
+          </svg>
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+import Header from "@/Components/header/Header";
 import Main from "@/Components/Main_Section/Main";
 
 export default function Home() {
   return (
     <div id="OpenCall">
+      <div className="vault">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          style={{ display: "none" }}
+        >
+          <symbol id="Capa_1" viewBox="0 0 197.091 197.091">
+            <path
+              d="M32.131,184.928L32.131,184.928c-18.388,0-31.573-2.505-32.131-2.616l0.734-7.648
+				c32.349,0,55.555-8.45,68.964-25.098c15.174-18.835,13.532-43.34,12.394-51.811H25.918l85.588-85.592l85.585,85.592h-53.976
+				C136.315,173.487,70.922,184.928,32.131,184.928z M44.564,90.028h43.912l0.673,3.028c0.311,1.432,7.476,35.341-13.381,61.302
+				c-8.425,10.475-20.113,18.041-34.94,22.651c42.867-1.882,90.753-18.714,94.861-83.362l0.229-3.618h42.527l-66.939-66.946
+				L44.564,90.028z"
+            ></path>
+          </symbol>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" style={{ display: "none" }}>
+          <symbol id="Capa_2" viewBox="0 0 640 512">
+            <path d="M41.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 41.4 86.6zM288 416H576c17.7 0 32 14.3 32 32s-14.3 32-32 32H288c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
+          </symbol>
+        </svg>
+      </div>
+      <Header />
       <Main />
     </div>
   );
